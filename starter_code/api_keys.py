6a549abc022dfab0a64af4d26204142d1420d606
Daniel Carrillo <danielcarrillo@Daniels-MacBook-Pro.local>
2020-07-28 00:06:55 -0600

    update

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "ea45174aae3e4fc5de1af5d14f74cd81"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCiyVKYg3FYX0fAR2S2RGR1m-kUN947W9s"
